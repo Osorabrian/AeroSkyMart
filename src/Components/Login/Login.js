862fc13bb0c19be2a988832a1863d950255b6654
Osorabrian <osorabrian@gmail.com>
2023-08-07 15:30:02 +0300

    install react router dom

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -5,7 +5,11 @@ export default class Login extends Component {
     render(){
         return(
             <form>
-                
+                <label>Username:</label>
+                <input type='text' placeholder='Enter Username' />
+                <label>Password:</label>
+                <input type='password' placeholder='Enter Password'/>
+                <button type='submit'>Log In</button>
             </form>
         )
     }
