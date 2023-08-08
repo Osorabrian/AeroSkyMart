07153ed2220aa5c10507ba43b21a178a089f173c
Osorabrian <osorabrian@gmail.com>
2023-08-08 15:51:01 +0300

    add nav links to navbar

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Login from '../Login/Login';
 import Home from '../Home/Home';
+import Navbar from '../Navbar/Navbar';
 import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Routes>
           <Route path='/' element={<Login/>} />
           <Route path='home' element={<Home/>} />
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
 
 export default function Navbar(){
     return(
-        <nav>
-            
+        <nav className='bg-[rgb(0,0,0)] text-light navbar'>
+            <NavLink className='navbar-brand text-light' to='/'>AeroSkyMart</NavLink>
+            <div>
+                <ul className='d-flex'>
+                    <li className='nav-item me-2'>
+                        <NavLink to='/home' className={'nav-link'}>Home</NavLink>
+                    </li>
+                    <li className='nav-item me-2'>
+                        <NavLink to='/store' className={'nav-link'}>Store</NavLink>
+                    </li>
+                    <li className='nav-item me-2'>
+                        <NavLink to='/cart' className={'nav-link'}>Cart</NavLink>
+                    </li>
+                    <li className='nav-item me-2'>
+                        <NavLink to='/login' className={'nav-link'}>LogIn</NavLink>
+                    </li>
+                </ul>
+            </div>
         </nav>
     )
 }
