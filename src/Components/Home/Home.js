685d1bd16c83aa0b3b4eef06ae391d7634399588
Osorabrian <osorabrian@gmail.com>
2023-08-09 13:32:46 +0300

    format landing page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -8,11 +8,11 @@ export default function Home() {
         <>
             <div id='headline'>
 
-                <div id='headline-message'>
+                <div id='headline-message' className="text-left">
 
-                   <h1 className="text-6xl text-light font-medium leading-normal text-left ">Elevate Your Aviation Experience !</h1>
+                   <h1 className="text-6xl text-light font-semibold leading-normal mt-3">Elevate Your Aviation Experience!</h1>
 
-                    <button type="button" id='store-button'>
+                    <button type="button" id='store-button' className="mt-5 mb-5 ms-3">
                         <div className="row">
                             <span className="col-9">
                             Go To Store  
