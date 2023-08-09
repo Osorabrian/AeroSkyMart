2f93a466484dedaa59806f2704c8c07a5ae7a782
Osorabrian <osorabrian@gmail.com>
2023-08-09 12:49:35 +0300

    add headline

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import gulfStream from '../images/gulfstream.jpg'
 import {FaOpencart} from 'react-icons/fa'
 import './home.css'
 
@@ -7,17 +6,23 @@ export default function Home() {
 
     return(
         <>
-            <img src={gulfStream} alt='headline' id='headline-photo' style={{zIndex: '-2'}}/>
-            <div id='headline-message'>
-                <h1 className="text-6xl text-light font-medium leading-normal">Elevate Your Aviation Experience !</h1>
-                <button type="button" id='store-button'>
-                    <div className="row">
-                        <span className="col-9">
-                           Go To Store  
-                        </span>
-                        <FaOpencart className="col-3 mt-1"/>
-                    </div>
-                </button>
+            <div id='headline'>
+
+                <div id='headline-message'>
+
+                   <h1 className="text-6xl text-light font-medium leading-normal text-left ">Elevate Your Aviation Experience !</h1>
+
+                    <button type="button" id='store-button'>
+                        <div className="row">
+                            <span className="col-9">
+                            Go To Store  
+                            </span>
+                            <FaOpencart className="col-3 mt-1"/>
+                        </div>
+                    </button> 
+
+                </div>
+                
             </div>
         </>
         
