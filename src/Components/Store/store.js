5c3c8526daa416bac2f9c6da0061a5381054bcae
Osorabrian <osorabrian@gmail.com>
2023-08-10 12:00:10 +0300

    format store page

--- src/Components/Store/store.js
+++ src/Components/Store/store.js
@@ -10,9 +10,9 @@ export default function Store(){
                 <h1 className="text-4xl">Our Products</h1>
             </div>
             
-            <div className="row" id='search-row'>
-                <input placeholder="Search" type="text" className="form-control w-40 rounded-0 col-9 ms-11"/>
-                <p className="text-right me-3 col-4"><i className="fa-solid fa-filter mt-3" style={{color: "#e96f0c"}}></i>   Filter</p>
+            <div className="row mt-3 ml-11" id='search-row'>
+                <input placeholder="Search" type="text" className="form-control w-50 rounded-0 col-9 ms-11"/>
+                <p className="text-right me-3 col-3"><i className="fa-solid fa-filter mt-3" style={{color: "#e96f0c"}}></i>   Filter</p>
             </div>
 
             <div className="row mx-auto mt-4">
