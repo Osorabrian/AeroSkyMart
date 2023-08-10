04cbf6d5def138971ce6175ebbe52647c2f7c1ee
Osorabrian <osorabrian@gmail.com>
2023-08-10 11:02:33 +0300

    edit store page

--- src/Components/Store/store.js
+++ src/Components/Store/store.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ProductCard from './ProductCard'
+import './store.css'
 
 export default function Store(){
 
@@ -7,12 +8,18 @@ export default function Store(){
         <>
             <div className=" mt-3">
                 <h1 className="text-4xl">Our Products</h1>
-                <p className="text-right me-3"><i class="fa-solid fa-filter mt-3" style={{color: "#e96f0c"}}></i>   Filter Products</p>
             </div>
+            
+            <div className="row" id='search-row'>
+                <input placeholder="Search" type="text" className="form-control w-40 rounded-0 col-9 ms-11"/>
+                <p className="text-right me-3 col-4"><i className="fa-solid fa-filter mt-3" style={{color: "#e96f0c"}}></i>   Filter</p>
+            </div>
+
             <div className="row mx-auto mt-4">
                 <ProductCard />
                 <ProductCard />
             </div>
+
         </>
     )
 }
