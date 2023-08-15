049829da930dba6405cc71f218ea78ef8602e60f
Osorabrian <osorabrian@gmail.com>
2023-08-15 16:54:06 +0300

    remove locl storage

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,25 +1,18 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import { useNavigate } from 'react-router-dom'
 
 export default function Login(){
 
     const navigate = useNavigate()
-    const logins = JSON.parse(localStorage.getItem('logins'))
 
-    const [userName, setUserName] = useState(logins.userName || "")
-    const [password, setPassword] = useState(logins.password || "")
-    const formData = {userName, password}
+    const [userName, setUserName] = useState('')
+    const [password, setPassword] = useState('')
 
     function logInFunction(e){
         e.preventDefault()
         console.log({userName, password})
         navigate('/home')
-        localStorage.removeItem('logins')
     }
-
-    useEffect(() => {
-        localStorage.setItem('logins', JSON.stringify(formData))
-    }, [formData])
 
         return(
 
@@ -29,12 +22,12 @@ export default function Login(){
 
                 <div className='mt-5 mb-3'>
                     <label className={'form-label'}>Username:</label>
-                    <input type='text' placeholder='Enter Username' value={userName} className='form-control' onChange={e => setUserName(e.target.value)}/>
+                    <input type='text' placeholder='Enter Username' className='form-control' onChange={e => setUserName(e.target.value)}/>
                 </div>
                 
                 <div className='mb-4'>
                     <label className='form-label'>Password:</label>
-                    <input type='password' placeholder={'Enter Password'} value={password} className='form-control' onChange={e => setPassword(e.target.value)}/>
+                    <input type='password' placeholder='Enter Password' className='form-control' onChange={e => setPassword(e.target.value)}/>
                 </div>
                 
                 <button type='submit' className='btn btn-primary' id='login-button'>Log In</button>
