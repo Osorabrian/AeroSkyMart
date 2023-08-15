ee12df4ff319f4c3ff315368d03f3525453ea0e5
Osorabrian <osorabrian@gmail.com>
2023-08-15 16:40:39 +0300

    add local storage for log in form

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,31 +1,40 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { useNavigate } from 'react-router-dom'
 
 export default function Login(){
 
     const navigate = useNavigate()
-    const [userName, setUserName] = useState('')
-    const [password, setPassword] = useState('')
+    const logins = JSON.parse(localStorage.getItem('logins'))
+
+    const [userName, setUserName] = useState(logins.userName || "")
+    const [password, setPassword] = useState(logins.password || "")
+    const formData = {userName, password}
 
     function logInFunction(e){
         e.preventDefault()
         console.log({userName, password})
         navigate('/home')
+        localStorage.removeItem('logins')
     }
 
+    useEffect(() => {
+        localStorage.setItem('logins', JSON.stringify(formData))
+    }, [formData])
+
         return(
+
             <form onSubmit={logInFunction} className='w-9/12 mx-auto text-left' id='login-form'>
 
                 <h1 className='text-6xl'>Log In</h1>
 
                 <div className='mt-5 mb-3'>
                     <label className={'form-label'}>Username:</label>
-                    <input type='text' placeholder='Enter Username' className='form-control' onChange={e => setUserName(e.target.value)}/>
+                    <input type='text' placeholder='Enter Username' value={userName} className='form-control' onChange={e => setUserName(e.target.value)}/>
                 </div>
                 
                 <div className='mb-4'>
                     <label className='form-label'>Password:</label>
-                    <input type='password' placeholder='Enter Password' className='form-control' onChange={e => setPassword(e.target.value)}/>
+                    <input type='password' placeholder={'Enter Password'} value={password} className='form-control' onChange={e => setPassword(e.target.value)}/>
                 </div>
                 
                 <button type='submit' className='btn btn-primary' id='login-button'>Log In</button>
