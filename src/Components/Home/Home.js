a98adf89f98d687b75fc76a9539f5c9a9bb13fb3
Osorabrian <osorabrian@gmail.com>
2023-08-16 14:48:09 +0300

    add link to store bitton

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {FaOpencart} from 'react-icons/fa'
 import './home.css'
+import { Link } from "react-router-dom";
 
 export default function Home() {
 
@@ -12,14 +13,14 @@ export default function Home() {
 
                    <h1 className="text-6xl text-light font-semibold leading-normal mt-4">Elevate Your Aviation Experience!</h1>
 
-                    <button type="button" id='store-button' className="mt-5 mb-5 ms-3">
+                    <Link type="button" id='store-button' className="mt-5 mb-5 ms-3" to='/store'>
                         <div className="row">
                             <span className="col-9">
                             Go To Store  
                             </span>
                             <FaOpencart className="col-3 mt-1"/>
                         </div>
-                    </button> 
+                    </Link> 
 
                 </div>
                 
