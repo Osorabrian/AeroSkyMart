b7110a18832267c8af99127e747b445903cb1129
Osorabrian <osorabrian@gmail.com>
2023-08-16 15:51:25 +0300

    format log in page

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, NavLink } from 'react-router-dom'
+import {FaFighterJet} from 'react-icons/fa'
+import './Login.css'
 
 export default function Login(){
 
@@ -16,21 +18,37 @@ export default function Login(){
 
         return(
 
-            <form onSubmit={logInFunction} className='w-9/12 mx-auto text-left' id='login-form'>
+            <form onSubmit={logInFunction} className='w-9/12 mx-auto text-left mt-5 p-4' id='login-form'>
 
-                <h1 className='text-6xl'>Log In</h1>
+                <h1 className='text-6xl text-center'>Log In</h1>
 
                 <div className='mt-5 mb-3'>
                     <label className={'form-label'}>Username:</label>
-                    <input type='text' placeholder='Enter Username' className='form-control' onChange={e => setUserName(e.target.value)}/>
+                    <input type='text' placeholder='Enter Username' className='form-control' onChange={e => setUserName(e.target.value)} required/>
                 </div>
                 
-                <div className='mb-4'>
+                <div>
                     <label className='form-label'>Password:</label>
-                    <input type='password' placeholder='Enter Password' className='form-control' onChange={e => setPassword(e.target.value)}/>
+                    <input type='password' placeholder='Enter Password' className='form-control' onChange={e => setPassword(e.target.value)} required/>
                 </div>
-                
-                <button type='submit' className='btn btn-primary' id='login-button'>Log In</button>
+
+                <div className='mb-3 text-right'>
+                    <NavLink className='form-text text-[#ff9800]'>Forgot Password ?</NavLink>
+                </div>
+
+                <div className='text-center'>
+                    <button type='submit' className='p-2 border-1 border-black' id='login-button'>
+                        <div className='row'>
+                            <span className='col-9'>Log In</span>
+                            <FaFighterJet className='col-3 mt-1 -ml-8'/>
+                        </div>
+                    </button>
+                </div>
+
+                <div className='mt-3 text-center'>
+                    <p>Don't have an acount ? <NavLink to='/signup' className='text-[#ff9800]'>Create Account.</NavLink></p>
+                </div>
+               
 
             </form>
         )
