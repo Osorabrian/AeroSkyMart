9d0ef863b7a4dd13568e803ee36b34f93d6a96c5
Osorabrian <osorabrian@gmail.com>
2023-08-19 13:25:08 +0300

    add search bar

--- src/Components/Store/store.js
+++ src/Components/Store/store.js
@@ -9,13 +9,13 @@ export default function Store(){
             <div className=" mt-3">
                 <h1 className="text-4xl">Our Products</h1>
             </div>
-            
-            {/* <div className="row mt-3 ml-11" id='search-row'>
-                <input placeholder="Search" type="text" className="form-control w-50 rounded-0 col-9 ms-11"/>
-                <p className="text-right me-3 col-3"><i className="fa-solid fa-filter mt-3" style={{color: "#e96f0c"}}></i>   Filter</p>
-            </div> */}
+             
+            <div className="row mt-3 ml-11  ms-auto mr-3 mb-3" id='search-row'>
+                <input placeholder="Search" type="text" className="form-control w-50 rounded-0 col-9 ms-11" id='search'/>
+                <p className="text-right col-3"><i className="fa-solid fa-filter mt-3" style={{color: "#e96f0c"}}></i>   Filter</p>
+            </div>
 
-            <div className="row grid grid-cols-5 mx-auto mt-5" id='cards'>
+            <div className="row mx-auto" id='cards'>
                 <ProductCard />
                 <ProductCard />
                 <ProductCard />
