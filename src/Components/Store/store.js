6253c65a2b2d94b27a0f891de9951a6a90ec4252
Osorabrian <osorabrian@gmail.com>
2023-08-19 20:54:07 +0300

    add searc functionality

--- src/Components/Store/store.js
+++ src/Components/Store/store.js
@@ -5,6 +5,7 @@ import './store.css'
 export default function Store(){
 
     const [data, setData] = useState([])
+    const [search, setSearch] = useState('')
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/albums')
@@ -12,20 +13,26 @@ export default function Store(){
         .then(data => setData(data))
     },[])
 
-    console.log(data)
-    const dataList = data.map((data) => {
+    const filteredData = data.filter((item) => {
+        return item.title.toLowerCase().includes(search.toLowerCase())
+    })
+
+    console.log(filteredData)
+
+    const dataList = filteredData.map((data) => {
         return <ProductCard key={data.id} name={data.title}/>
     })
 
     return(
         <div className="font-lato">
+
             <div className=" mt-3">
                 <h1 className="text-4xl">Our Products</h1>
             </div>
              
             <div className="row mt-3 mb-3" id='search-row'>
                 <div className="row ms-auto" id='search-bar'>
-                    <input placeholder="Search..." type="search" className="form-control w-50 rounded-0 col-9 ms-11" id='search'/>
+                    <input placeholder="Search..." type="search" className="form-control w-50 rounded-0 col-9 ms-11" id='search' onChange={e => setSearch(e.target.value)}/>
                     <p className="text-right col-3 -mt-1"><i className="fa-solid fa-filter mt-3" style={{color: "#e96f0c"}}></i>   Filter</p>
                 </div>
             </div>
