04fadfed82aa0008ed4cb91a473582c2cebed8fe
Osorabrian <osorabrian@gmail.com>
2023-08-19 13:47:26 +0300

    edit search bar

--- src/Components/Store/store.js
+++ src/Components/Store/store.js
@@ -10,9 +10,11 @@ export default function Store(){
                 <h1 className="text-4xl">Our Products</h1>
             </div>
              
-            <div className="row mt-3 ml-11  ms-auto mb-3" id='search-row'>
-                <input placeholder="Search" type="text" className="form-control w-50 rounded-0 col-9 ms-11" id='search'/>
-                <p className="text-right col-3"><i className="fa-solid fa-filter mt-3" style={{color: "#e96f0c"}}></i>   Filter</p>
+            <div className="row mt-3 mb-3" id='search-row'>
+                <div className="row ms-auto" id='search-bar'>
+                    <input placeholder="Search" type="text" className="form-control w-50 rounded-0 col-9 ms-11" id='search'/>
+                    <p className="text-right col-3"><i className="fa-solid fa-filter mt-3" style={{color: "#e96f0c"}}></i>   Filter</p>
+                </div>
             </div>
 
             <div className="row mx-auto" id='cards'>
