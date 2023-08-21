926b2f7e4b5c267d423af82a5229d93a9174aaa6
Osorabrian <osorabrian@gmail.com>
2023-08-21 12:19:59 +0300

    add filter div

--- src/Components/Store/store.js
+++ src/Components/Store/store.js
@@ -6,6 +6,7 @@ export default function Store(){
 
     const [data, setData] = useState([])
     const [search, setSearch] = useState('')
+    const [filter, setFilter] = useState(false)
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/albums')
@@ -13,11 +14,19 @@ export default function Store(){
         .then(data => setData(data))
     },[])
 
+    function handleFilter(){
+        if(filter){
+            setFilter(false)
+        }else{
+            setFilter(true)
+        }
+    }
+
+    console.log(filter)
+
     const filteredData = data.filter((item) => {
         return item.title.toLowerCase().includes(search.toLowerCase())
     })
-
-    console.log(filteredData)
 
     const dataList = filteredData.map((data) => {
         return <ProductCard key={data.id} name={data.title}/>
@@ -33,9 +42,33 @@ export default function Store(){
             <div className="row mt-3 mb-3" id='search-row'>
                 <div className="row ms-auto" id='search-bar'>
                     <input placeholder="Search..." type="search" className="form-control w-50 rounded-0 col-9 ms-11" id='search' onChange={e => setSearch(e.target.value)}/>
-                    <p className="text-right col-3 -mt-1"><i className="fa-solid fa-filter mt-3" style={{color: "#e96f0c"}}></i>   Filter</p>
+                    <button className="text-right col-3 -mt-1" onClick={handleFilter}><i className="fa-solid fa-filter mt-3" style={{color: "#e96f0c"}}></i>   Filter</button>
                 </div>
             </div>
+
+                <div className="row mx-auto" id='filter-div'>
+
+                    <div className="col-4" id='search-bar'>
+                        <input placeholder="Keyword" type="search" className="form-control w-50 rounded-0" id='search' onChange={e => setSearch(e.target.value)}/>
+                    </div>
+
+                    <div className="col-4">
+                        <select name='category' id='category-select' className="form-select search">
+                            <option value=''>Select a category</option>
+                            <option>Books</option>
+                            <option>Accessories</option>
+                            <option>Clothes</option>
+                            <option>Souvenior</option>
+                        </select>
+                    </div>
+
+                    <div className="col-4">
+                        <label className="form-label">Price</label>
+                        <input type='number' placeholder="Min" classname='form-control' id={'min-price'}/>
+                        <input type='number' placeholder="Max" classname='form-control' id={'max-price'}/>
+                    </div>
+
+                </div>
 
             <div className="row mx-auto" id='cards'>
                 {dataList}
