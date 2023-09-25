db76a9686f5abc5ad2ee01903be93f1c7355c93f
Osorabrian <osorabrian@gmail.com>
2023-09-25 16:35:04 +0300

    format carousel

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+import './product.css'
+import {FcPrevious, FcNext} from 'react-icons/fc'
 import img1 from '../images/msfs/msfs_cover.jpg'
 import img2 from '../images/msfs/msfs_cockpit.jpg'
-import img3 from '../images/msfs/msfs_1.jpg'
+import img3 from '../images/msfs/msfs_2.jpg'
 import img4 from '../images/msfs/msfs_3.jpeg'
 
 export default function Product(){
@@ -18,17 +20,22 @@ export default function Product(){
     
     return(
         <>
-            <div id="#carousel">
+            <div id="carousel">
 
-                <img src={images[currentImage]} alt="msfs" className="mt-4 p-3"/>
+                <div className="carousel-image">
+                    <img src={images[currentImage]} alt="msfs" className="mt-4 p-3"/>
 
-                <button type='button' onClick={() => {setCurrentImage(currentImage + 1)}}>
-                    next
-                </button>
+                    <button type='button' className="next-button" onClick={() => {setCurrentImage(currentImage + 1)}}>
+                        <FcNext className="text-4xl hover:scale-150"/>
+                    </button>
 
-                <button type='button' onClick={() => setCurrentImage(currentImage - 1)}>
-                    previous
-                </button>
+                    <button type='button' className="prev-button" onClick={() => setCurrentImage(currentImage - 1)}>
+                        <FcPrevious className="text-4xl hover:scale-150"/>
+                    </button>
+                </div>
+                
+
+                <p>{currentImage + 1}/{images.length}</p>
             </div>
 
             <h1>Microsoft Simulator</h1>
