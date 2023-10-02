4227dd20ad0397152e3f48282f0bd7fc83aa9d65
Osorabrian <osorabrian@gmail.com>
2023-10-02 10:21:10 +0300

    create side nav bar

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,11 +6,13 @@ import Store from '../Store/store';
 import SignUp from '../Signup/SignUp';
 import Product from '../Product/Product'
 import {Routes, Route} from 'react-router-dom'
+import SideNav from '../Navbar/SideNav';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <SideNav/>
       <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='store' element={<Store/>} />
---
+++ src/Components/Navbar/SideNav.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import {FaHome} from 'react-icons/fa'
+import {BiStore} from 'react-icons/bi'
+import {AiOutlineLogin, AiOutlineShoppingCart, AiOutlineUserAdd} from 'react-icons/ai'
+import './Navbar.css'
+
+export default function SideNav(){
+    return(
+        <div>
+            <ul id='side-nav'>
+                <li className='nav-item row'>
+                    <FaHome className='col-6 mt-1'/>
+                    <NavLink to='/' className={'nav-link col-6 -ms-2'}>Home</NavLink>
+                </li>
+                <li className='nav-item ms-4 row'>
+                    <BiStore className='col-6 mt-1'/>
+                    <NavLink to='/store' className={'nav-link col-6 -ms-2'}>Store</NavLink>
+                </li>
+                <li className='nav-item ms-4 row'>
+                    <AiOutlineShoppingCart className='col-6 mt-1 text-3xl'/>
+                    <NavLink to='/cart' className={'nav-link col-6 -ms-2 mt-1'}>Cart</NavLink>
+                </li>
+                <li className='nav-item ms-4 row'>
+                    <AiOutlineLogin className='col-5 mt-1 text-3xl'/>
+                    <NavLink to='/login' className={'nav-link col-6 -ms-2 mt-1'}>Login</NavLink>
+                </li>   
+                <li className='nav-item ms-4 row'>
+                    <AiOutlineUserAdd className='col-5 mt-1'/>
+                    <NavLink to='/signup' className={'nav-link col-6 -ms-2'}>SignUp</NavLink>
+                </li>
+            </ul>
+        </div>
+    )
+}
