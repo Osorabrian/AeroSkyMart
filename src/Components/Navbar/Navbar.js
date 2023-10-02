3550ad2a5764ff9c864ae337b3f7c6ac9ee78b15
Osorabrian <osorabrian@gmail.com>
2023-10-02 16:10:05 +0300

    make navbar responsive

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {FaHome} from 'react-icons/fa'
 import {BiStore} from 'react-icons/bi'
 import {AiOutlineLogin, AiOutlineShoppingCart, AiOutlineUserAdd} from 'react-icons/ai'
-import {CiMenuFries} from 'react-icons/ci'
+import {CiMenuBurger} from 'react-icons/ci'
 import logo from '../images/plane.png'
 import {NavLink} from 'react-router-dom'
 import './Navbar.css'
@@ -12,43 +12,42 @@ export default function Navbar(){
         <nav className='bg-[rgb(0,0,0)] text-light navbar font-roboto navbar-expand-lg' id='nav-bar'>
             <div className='container-fluid'>
 
+            <button id='side-menu'>
+                <CiMenuBurger className='text-2xl me-5'/>   
+            </button>
+
             <NavLink className='navbar-brand text-light text-2xl me-auto row' to='/'>
                 <img src={logo} alt='logo'id='logo' className='col-6 -mt-1'/>
                 <span className='col-6 -ml-4 font-roboto'>AeroSkyMart</span>
             </NavLink>
 
-            {/* <nav className='navbar mx-auto d-flex text-xl text-light'>
-                <li className='nav-item row'>
-                    <FaHome className='col-6 mt-1'/>
-                    <NavLink to='/' className={'nav-link col-6 -ms-2'}>Home</NavLink>
-                </li>
-                <li className='nav-item ms-4 row'>
-                    <BiStore className='col-6 mt-1'/>
-                    <NavLink to='/store' className={'nav-link col-6 -ms-2'}>Store</NavLink>
-                </li>
-                <li className='nav-item ms-4 row'>
-                    <AiOutlineShoppingCart className='col-6 mt-1 text-3xl'/>
-                    <NavLink to='/cart' className={'nav-link col-6 -ms-2 mt-1'}>Cart</NavLink>
-                </li> 
-            </nav>  
+                <div className='horizontal-nav navbar mx-auto flex text-xl text-light'>
+                    <li className='nav-item row'>
+                        <FaHome className='col-6 mt-1'/>
+                        <NavLink to='/' className={'nav-link col-6 -ms-2'}>Home</NavLink>
+                    </li>
+                    <li className='nav-item ms-4 row'>
+                        <BiStore className='col-6 mt-1'/>
+                        <NavLink to='/store' className={'nav-link col-6 -ms-2'}>Store</NavLink>
+                    </li>
+                    <li className='nav-item ms-4 row'>
+                        <AiOutlineShoppingCart className='col-6 mt-1 text-3xl'/>
+                        <NavLink to='/cart' className={'nav-link col-6 -ms-2 mt-1'}>Cart</NavLink>
+                    </li> 
+                </div>  
 
-            <nav className='navbar d-flex ms-auto text-xl'>
-                <li className='nav-item ms-4 row'>
-                    <AiOutlineLogin className='col-5 mt-1 text-3xl'/>
-                    <NavLink to='/login' className={'nav-link col-6 -ms-2 mt-1'}>Login</NavLink>
-                </li>   
-                <li className='nav-item ms-4 row'>
-                    <AiOutlineUserAdd className='col-5 mt-1'/>
-                    <NavLink to='/signup' className={'nav-link col-6 -ms-2'}>SignUp</NavLink>
-                </li>    
-            </nav>   */}
-
-           
-            <button>
-                <CiMenuFries className='text-2xl'/>   
-            </button>
-
-            </div>          
+                <div className='horizontal-nav navbar flex ms-auto text-xl'>
+                    <li className='nav-item ms-4 row'>
+                        <AiOutlineLogin className='col-5 mt-1 text-3xl'/>
+                        <NavLink to='/login' className={'nav-link col-6 -ms-2 mt-1'}>Login</NavLink>
+                    </li>   
+                    <li className='nav-item ms-4 row'>
+                        <AiOutlineUserAdd className='col-5 mt-1'/>
+                        <NavLink to='/signup' className={'nav-link col-6 -ms-2'}>SignUp</NavLink>
+                    </li>    
+                </div>
+            </div>
+          
         </nav>
     )
 }
