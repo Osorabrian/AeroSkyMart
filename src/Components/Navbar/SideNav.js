3939df20e06879de3766e72f07e62792bbd453d2
Osorabrian <osorabrian@gmail.com>
2023-10-02 10:40:56 +0300

    format side nav

--- src/Components/Navbar/SideNav.js
+++ src/Components/Navbar/SideNav.js
@@ -8,24 +8,24 @@ import './Navbar.css'
 export default function SideNav(){
     return(
         <div>
-            <ul id='side-nav'>
+            <ul id='side-nav' className="text-light text-xl">
                 <li className='nav-item row'>
                     <FaHome className='col-6 mt-1'/>
                     <NavLink to='/' className={'nav-link col-6 -ms-2'}>Home</NavLink>
                 </li>
-                <li className='nav-item ms-4 row'>
+                <li className='nav-item row'>
                     <BiStore className='col-6 mt-1'/>
                     <NavLink to='/store' className={'nav-link col-6 -ms-2'}>Store</NavLink>
                 </li>
-                <li className='nav-item ms-4 row'>
+                <li className='nav-item row'>
                     <AiOutlineShoppingCart className='col-6 mt-1 text-3xl'/>
                     <NavLink to='/cart' className={'nav-link col-6 -ms-2 mt-1'}>Cart</NavLink>
                 </li>
-                <li className='nav-item ms-4 row'>
+                <li className='nav-item row'>
                     <AiOutlineLogin className='col-5 mt-1 text-3xl'/>
                     <NavLink to='/login' className={'nav-link col-6 -ms-2 mt-1'}>Login</NavLink>
                 </li>   
-                <li className='nav-item ms-4 row'>
+                <li className='nav-item row'>
                     <AiOutlineUserAdd className='col-5 mt-1'/>
                     <NavLink to='/signup' className={'nav-link col-6 -ms-2'}>SignUp</NavLink>
                 </li>
