47272cf491af9f41b5ef0285a4bbb24faa0019c8
Osorabrian <osorabrian@gmail.com>
2023-10-02 15:20:50 +0300

    format side navbar

--- src/Components/Navbar/SideNav.js
+++ src/Components/Navbar/SideNav.js
@@ -11,27 +11,27 @@ export default function SideNav(){
                 <hr></hr>
                 <li className='nav-item row'>
                     <FaHome className='col-6 mt-1'/>
-                    <NavLink to='/' className={'nav-link col-6 -ms-11'}>Home</NavLink>
+                    <NavLink to='/' className={'nav-link col-6 -ms-14'}>Home</NavLink>
                 </li>
                 <hr></hr>
                 <li className='nav-item row'>
                     <BiStore className='col-6 mt-1'/>
-                    <NavLink to='/store' className={'nav-link col-6 -ms-11'}>Store</NavLink>
+                    <NavLink to='/store' className={'nav-link col-6 -ms-14'}>Store</NavLink>
                 </li>
                 <hr></hr>
                 <li className='nav-item row'>
-                    <AiOutlineShoppingCart className='col-6 mt-1 text-2xl'/>
-                    <NavLink to='/cart' className={'nav-link col-6 -ms-11 mt-1'}>Cart</NavLink>
+                    <AiOutlineShoppingCart className='col-6 mt-1 text-3xl'/>
+                    <NavLink to='/cart' className={'nav-link col-6 -ms-14 mt-1'}>Cart</NavLink>
                 </li>
                 <hr></hr>
                 <li className='nav-item row'>
-                    <AiOutlineLogin className='col-5 mt-1 text-2xl ms-2'/>
-                    <NavLink to='/login' className={'nav-link col-6 -ms-9 mt-1'}>Log In</NavLink>
-                </li>   
-                <hr></hr>
+                    <AiOutlineLogin className='col-5 mt-1 text-3xl ms-1'/>
+                    <NavLink to='/login' className={'nav-link col-6 -ms-12 mt-1'}>Login</NavLink>
+                </li> 
+                <hr></hr>  
                 <li className='nav-item row'>
-                    <AiOutlineUserAdd className='col-5 mt-1 ms-2'/>
-                    <NavLink to='/signup' className={'nav-link col-6 -ms-9'}>Sign Up</NavLink>
+                    <AiOutlineUserAdd className='col-5 mt-1 ms-1'/>
+                    <NavLink to='/signup' className={'nav-link col-6 -ms-12'}>SignUp</NavLink>
                 </li>
                 <hr></hr>
             </ul>
