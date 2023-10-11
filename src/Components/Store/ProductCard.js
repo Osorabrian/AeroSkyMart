9554ec262094f1959553665ad67edd502c2c582a
Osorabrian <osorabrian@gmail.com>
2023-10-11 12:48:38 +0300

    format product cards

--- src/Components/Store/ProductCard.js
+++ src/Components/Store/ProductCard.js
@@ -11,7 +11,7 @@ export default function ProjectCard({name,id}){
     return(
         <Link className="card mb-4 px-0" id='product-card' to ={`${id}`} >
             <img src={gulf} alt='lt' className="card-img-top px-0 flex-0"/>
-            <div className="card-body text-left -ms-4 -mt-2">
+            <div className="card-body text-left -ms-3 -mt-2">
                 <p className='card-text mb-1 font-semibold'>{name}</p>
                 <div className="row mb-1">
                     {/* <BsCheck2 className="fill-[#4caf50] col-3 mt-1 -ml-3 icon"/><p className="col-9 -ml-6 card-text stock">5 in Stock</p> */}
