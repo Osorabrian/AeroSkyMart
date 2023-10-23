5cee250cae42b27d3fb9147bf395b2c06d84c9e8
Osorabrian <osorabrian@gmail.com>
2023-10-23 15:16:44 +0300

    ad hot delas section

--- src/Components/Home/Deals.js
+++ src/Components/Home/Deals.js
@@ -6,7 +6,7 @@ export default function Deals(){
         <>
             <h1 className='text-4xl font-semibold'>Hot Deals</h1>
 
-            <div className='flex flex-grow mt-5'>
+            <div className='row mt-5'>
                 <ProductCard/>
                 <ProductCard/>
                 <ProductCard/>
