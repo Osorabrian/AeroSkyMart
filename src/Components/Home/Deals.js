83d5d8f2085ce9518c449c54a64e34e6989f2b40
Osorabrian <osorabrian@gmail.com>
2023-10-23 16:41:39 +0300

    edit hot deals section

--- src/Components/Home/Deals.js
+++ src/Components/Home/Deals.js
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {Link} from 'react-router-dom'
 import {BsArrowRight} from 'react-icons/bs'
 import ProductCard from '../Store/ProductCard'
 
 export default function Deals(){
+
+    const [cardList, setCardList] = useState([])
+
+    useEffect(()=> {
+        fetch('https://jsonplaceholder.typicode.com/albums')
+        .then(res => res.json())
+        .then(data => setCardList(data))
+    },[])
+
     return(
         <>
             <h1 className='text-4xl font-semibold'>Hot Deals</h1>
 
             <div className='row mt-5 m-2'>
-                <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
+                {cardList.slice(0,4).map((product) => {
+                    return <ProductCard key={product.id} name={product.title} id={product.id}/>
+                })}
             </div>
 
             <Link to='/store' className='flex text-[rgb(0,0,255)] font-semibold me-5 mb-5' style={{float: 'right'}}>
