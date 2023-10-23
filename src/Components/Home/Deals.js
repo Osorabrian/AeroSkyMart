25a6ab81f2be8fe47ffc66ad4e33f1e9cfa55f51
Osorabrian <osorabrian@gmail.com>
2023-10-23 16:24:28 +0300

    edit hot deals section

--- src/Components/Home/Deals.js
+++ src/Components/Home/Deals.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {BsArrowRight} from 'react-icons/bs'
 import ProductCard from '../Store/ProductCard'
 
 export default function Deals(){
@@ -9,7 +11,14 @@ export default function Deals(){
             <div className='row mt-5 m-2'>
                 <ProductCard/>
                 <ProductCard/>
+                <ProductCard/>
+                <ProductCard/>
             </div>
+
+            <Link to='/store' className='flex text-[rgb(0,0,255)] font-semibold me-5 mb-5' style={{float: 'right'}}>
+                View All 
+                <BsArrowRight className='my-auto ms-2'/> 
+            </Link>
         </>
     )
 }
