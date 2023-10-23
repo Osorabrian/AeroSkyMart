354984f1d44bb1ca2804e9aaa3351f9321d03d9c
Osorabrian <osorabrian@gmail.com>
2023-10-23 11:16:50 +0300

    add images to category section

--- src/Components/Home/Category.js
+++ src/Components/Home/Category.js
@@ -1,17 +1,28 @@
 import React from "react";
+import './home.css'
 import merchandise from '../images/categories/merch.webp'
+import simulator from '../images/msfs/msfs_cover.jpg'
+import books from '../images/categories/books.webp'
+import uniform from '../images/categories/uniform.webp'
+import collectables from '../images/categories/collectables.webp'
+import wall_decor from '../images/categories/wall decor.webp'
 
 export default function Category(){
 
+    const categories_images = [merchandise, simulator, books, uniform, collectables, wall_decor]
+
     return(
-        <div>
+        <div className="mt-5">
             <h1>Categories</h1>
 
-            <div>
-                <div>
-                    <img src={merchandise} alt='merchandise' className="category-image"/>
-                </div>
-
+            <div className="row">
+                { categories_images.map((category) => {
+                    return(
+                        <div className="col-2">
+                            <img src={category} alt={`${category} thumbnail`} className="category-image"/>
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )
