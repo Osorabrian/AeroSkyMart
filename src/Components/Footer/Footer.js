3b07d4d9bb0c9cc182cb82f78c039f12945c9619
Osorabrian <osorabrian@gmail.com>
2023-10-24 09:48:52 +0300

    create footer css file

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,8 +1,9 @@
 import React from "react";
+import './footer.css'
 
 export default function Footer(){
     return(
-        <div>
+        <div id="footer-div">
             <h1>This is the footer</h1>
         </div>
     )
