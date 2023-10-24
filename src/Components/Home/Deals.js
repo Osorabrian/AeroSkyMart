4bf645deb00b3ebfedb8a0251546e58f8dff2a60
Osorabrian <osorabrian@gmail.com>
2023-10-24 16:15:36 +0300

    format deals section

--- src/Components/Home/Deals.js
+++ src/Components/Home/Deals.js
@@ -16,21 +16,21 @@ export default function Deals(){
     return(
         <div className='bg-[#a8acab] mt-5'>
 
-                <div className='row m-2'>
+                <div className='row m-2 mb-5'>
 
-                    <div className='flex'>
-                        <h1 className='text-4xl font-semibold mt-3 mb-4 mx-auto'>Hot Deals</h1>
-
-                        <Link className='flex text-[rgb(0,0,255)] font-semibold m-3' style={{float: 'right'}}>
-                            View More 
-                            <BsArrowRight className=' mt-1 ms-2'/> 
-                        </Link>
-                    </div>
+                <h1 className='text-4xl font-semibold mt-3 mb-4'>Hot Deals</h1>
 
                     {cardList.slice(0,4).map((product) => {
                         return <ProductCard key={product.id} name={product.title} id={product.id}/>
                     })}
                 </div>
+                
+                <Link className='flex text-[rgb(0,0,255)] font-semibold justify-center'>
+                    <div className='m-3 flex'>
+                        <span className='-mt-5'>View More</span> 
+                        <BsArrowRight className='-mt-4 ms-2 '/> 
+                    </div>
+                </Link>
                         
         </div>
     )
