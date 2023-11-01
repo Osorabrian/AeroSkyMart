cb4065d20995adb6b5fa9c71403bd3dec664336c
Osorabrian <osorabrian@gmail.com>
2023-11-01 16:17:13 +0300

    edit my account section in footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 import './footer.css'
+import logo from '../images/plane.png'
 import {TfiEmail} from 'react-icons/tfi'
 import {PiPhoneCallThin} from 'react-icons/pi'
-import {BsBoxArrowUpRight} from 'react-icons/bs'
 import {BsMeta} from 'react-icons/bs'
 import {BsYoutube} from 'react-icons/bs'
 import {BsInstagram} from 'react-icons/bs'
@@ -17,18 +17,60 @@ export default function Footer(){
                     {logo: BsInstagram, color: '#C13584', link: 'https://www.instagram.com/'}
                     ]
 
+    const [user, setUser] = useState(false)
+
+
     return(
         <div id="footer-div">
             
                 <div className="flex flex-wrap justify-between text-light text-left">
 
-                    <div className="flex-wrap m-4">
-                        <h1>Contacts</h1>
+                    <div className="flex m-3">
+                        <img src={logo} alt='logo'id='logo' style={{width: '50px', height: '30px'}}/>
+                        <span className='font-roboto text-xl font-semibold'>AeroSkyMart</span>
+                    </div>
 
-                        <div className="flex m-2">
-                            <BsBoxArrowUpRight className='my-auto'/>
-                            <span className="my-auto ms-2">Contact Us Page</span>
-                        </div>
+                    <div className="flex-wrap m-3">
+                        <h1>My Account</h1>
+                        { user ? (
+                                <>
+                                    <p className="m-1">My Cart</p>
+                                    <p className="m-1">My Orders</p>
+                                    <p className="m-1">My Wishlist</p>
+                                    <button className="m-2 btn bg-light text-dark rounded-none">
+                                        Log Out
+                                    </button>
+                                </>
+                        ):(
+                            <div className="mx-auto">
+                                <button className="m-3 btn bg-light text-dark rounded-none">
+                                    Log In
+                                </button>
+                                <p>Create an Account</p>
+                                <button className="m-3 btn bg-light text-dark rounded-none">
+                                    Sign Up
+                                </button>
+                            </div>
+                        )}
+                        
+                    </div>
+
+                    <div className="flex-wrap m-3">
+                        <h1>Customer Support</h1>
+
+                        <p className="m-2">FAQs</p>
+                        <p className="m-2">Shipping & Handling</p>
+                        <p className="m-2">Terms & Conditions</p>
+                        <p className="m-2">Privacy Policy</p>
+                        <p className=" m-2">Contact Us</p>
+
+                    </div>
+
+                    <div className="m-3">
+                        <h1>General</h1>
+
+                        <p className=" m-2">About Us</p>
+                        <p className=" m-2">Contact Us</p>
 
                         <div className="flex m-2">
                             <TfiEmail className="my-auto"/>
@@ -40,28 +82,16 @@ export default function Footer(){
                             <span className="m-auto ms-2">+254712345678</span>
                         </div>
 
-
                     </div>
 
-                    <div className="m-4">
-                        <h1>About</h1>
-
-                        <div className="flex-wrap">
-                            <div className="flex m-2">
-                                <BsBoxArrowUpRight className="my-auto"/>
-                                <span className='my-auto ms-2'>About Us Page</span>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="m-4">
+                    <div className="m-3">
                         <h1>Socials</h1>
 
                         <div className="flex flex-wrap m-2">
                             {socials.map((social, index)=> {
                                 return(
                                     <Link to={social.link} key={index}>
-                                        <social.logo className={`text-[${social.color}] my-auto m-2`} />
+                                        <social.logo className={`text-[${social.color}] my-auto m-2 text-xl`} />
                                     </Link>
                                 )
                             })} 
