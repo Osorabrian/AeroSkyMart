c2ba4346f66374147fec6a3bf26a6b340efb01af
Osorabrian <osorabrian@gmail.com>
2023-11-02 15:58:25 +0300

    format customer support component of footer

--- src/Components/Footer/Customer.js
+++ src/Components/Footer/Customer.js
@@ -1,16 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Customer(){
     return(
         <div className="flex-wrap m-3">
             <h1 className="text-lg font-semibold">Customer Support</h1>
-            <div className="text-[#B2BEB5]">
-                <p className="m-2">FAQs</p>
-                <p className="m-2">Shipping & Handling</p>
-                <p className="m-2">Terms & Conditions</p>
-                <p className="m-2">Privacy Policy</p>
-                <p className=" m-2">Contact Us</p>
-            </div>
+
+            <ul className="text-[#B2BEB5]">
+
+                <li className="m-2">
+                    <Link to='/'>FAQs</Link>
+                </li>
+                <li className="m-2">
+                    <Link to='/'>Shipping & Handling</Link>
+                </li>
+                <li className="m-2">
+                    <Link to='/'>Terms & Conditions</Link>
+                </li>
+                <li className="m-2">
+                    <Link to='/cart' >Privacy Policy</Link>
+                </li>
+                <li className="m-2">
+                    <Link to='/contact'>Contact Us</Link>
+                </li>
+
+            </ul>
+
         </div>
     )
 }
