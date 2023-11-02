3d041f71fb004357e0d8185e2d2d8a702f6e3971
Osorabrian <osorabrian@gmail.com>
2023-11-02 13:24:37 +0300

    add functionality to footer log in and sign up buttons

--- src/Components/Footer/Account.js
+++ src/Components/Footer/Account.js
@@ -1,8 +1,18 @@
 import React, {useState} from "react";
+import {Navigate, useNavigate} from 'react-router-dom'
 
 export default function Account(){
 
     const [user, setUser] = useState(false)
+    const navigate = useNavigate()
+
+    function logIn(){
+        navigate("/login")
+    }
+
+    function signUp(){
+        navigate('/signup')
+    }
 
     return(
         <div className="flex-wrap m-3">
@@ -18,11 +28,11 @@ export default function Account(){
                     </div>
             ):(
                 <div className="mx-auto text-[#B2BEB5]">
-                    <button className="m-3 btn bg-light text-dark rounded-none">
+                    <button className="m-3 btn bg-light text-dark rounded-none" onClick={logIn}>
                         Log In
                     </button>
                     <p>Don't have an Account ?</p>
-                    <button className="m-3 btn bg-light text-dark rounded-none">
+                    <button className="m-3 btn bg-light text-dark rounded-none" onClick={signUp}>
                         Create Account
                     </button>
                 </div>
