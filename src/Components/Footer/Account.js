101c26c185d70dbf6764f9e547a6c1743232a478
Osorabrian <osorabrian@gmail.com>
2023-11-02 13:57:31 +0300

    edit my account section in footer

--- src/Components/Footer/Account.js
+++ src/Components/Footer/Account.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {useNavigate} from 'react-router-dom'
+import {useNavigate, Link} from 'react-router-dom'
 
 export default function Account(){
 
@@ -7,17 +7,23 @@ export default function Account(){
     const navigate = useNavigate()
 
     return(
-        <div className="flex-wrap m-3">
+        <div className="m-3">
             <h1 className="text-lg font-semibold">My Account</h1>
             { user ? (
-                    <div className="text-[#B2BEB5]">
-                        <p className="m-1">My Cart</p>
-                        <p className="m-1">My Orders</p>
-                        <p className="m-1">My Wishlist</p>
+                    <ul className="text-[#B2BEB5]">
+                        <li className="m-2">
+                            <Link to='/cart'>My Cart</Link>
+                        </li>
+                        <li className="m-2">
+                            <Link to='/' >My Orders</Link>
+                        </li>
+                        <li className="m-2">
+                            <Link to='/'>My Wishlist</Link>
+                        </li>
                         <button className="m-2 btn bg-light text-dark rounded-none" onClick={()  => navigate('/')}>
                             Log Out
                         </button>
-                    </div>
+                    </ul>
             ):(
                 <div className="mx-auto text-[#B2BEB5]">
                     <button className="m-3 btn bg-light text-dark rounded-none" onClick={() => navigate('/login')}>
