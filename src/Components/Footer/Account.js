dfe1eca8e8eb6e9f6dc2021da6cb8da11c0c6a78
Osorabrian <osorabrian@gmail.com>
2023-11-02 13:30:01 +0300

    edit footer links

--- src/Components/Footer/Account.js
+++ src/Components/Footer/Account.js
@@ -1,18 +1,10 @@
 import React, {useState} from "react";
-import {Navigate, useNavigate} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom'
 
 export default function Account(){
 
     const [user, setUser] = useState(false)
     const navigate = useNavigate()
-
-    function logIn(){
-        navigate("/login")
-    }
-
-    function signUp(){
-        navigate('/signup')
-    }
 
     return(
         <div className="flex-wrap m-3">
@@ -22,17 +14,17 @@ export default function Account(){
                         <p className="m-1">My Cart</p>
                         <p className="m-1">My Orders</p>
                         <p className="m-1">My Wishlist</p>
-                        <button className="m-2 btn bg-light text-dark rounded-none">
+                        <button className="m-2 btn bg-light text-dark rounded-none" onClick={()  => navigate('/')}>
                             Log Out
                         </button>
                     </div>
             ):(
                 <div className="mx-auto text-[#B2BEB5]">
-                    <button className="m-3 btn bg-light text-dark rounded-none" onClick={logIn}>
+                    <button className="m-3 btn bg-light text-dark rounded-none" onClick={() => navigate('/login')}>
                         Log In
                     </button>
                     <p>Don't have an Account ?</p>
-                    <button className="m-3 btn bg-light text-dark rounded-none" onClick={signUp}>
+                    <button className="m-3 btn bg-light text-dark rounded-none" onClick={() => navigate('/signup')}>
                         Create Account
                     </button>
                 </div>
