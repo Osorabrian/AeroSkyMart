9ca05f2a6ccf69b132c97d6eaeee44905b078854
Osorabrian <osorabrian@gmail.com>
2023-11-02 09:50:59 +0300

    edit footer color

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -31,18 +31,18 @@ export default function Footer(){
                     </div>
 
                     <div className="flex-wrap m-3">
-                        <h1>My Account</h1>
+                        <h1 className="text-lg font-semibold">My Account</h1>
                         { user ? (
-                                <>
+                                <div className="text-[#B2BEB5]">
                                     <p className="m-1">My Cart</p>
                                     <p className="m-1">My Orders</p>
                                     <p className="m-1">My Wishlist</p>
                                     <button className="m-2 btn bg-light text-dark rounded-none">
                                         Log Out
                                     </button>
-                                </>
+                                </div>
                         ):(
-                            <div className="mx-auto">
+                            <div className="mx-auto text-[#B2BEB5]">
                                 <button className="m-3 btn bg-light text-dark rounded-none">
                                     Log In
                                 </button>
@@ -56,36 +56,38 @@ export default function Footer(){
                     </div>
 
                     <div className="flex-wrap m-3">
-                        <h1>Customer Support</h1>
+                        <h1 className="text-lg font-semibold">Customer Support</h1>
+                        <div className="text-[#B2BEB5]">
+                            <p className="m-2">FAQs</p>
+                            <p className="m-2">Shipping & Handling</p>
+                            <p className="m-2">Terms & Conditions</p>
+                            <p className="m-2">Privacy Policy</p>
+                            <p className=" m-2">Contact Us</p>
+                        </div>
+                    </div>
 
-                        <p className="m-2">FAQs</p>
-                        <p className="m-2">Shipping & Handling</p>
-                        <p className="m-2">Terms & Conditions</p>
-                        <p className="m-2">Privacy Policy</p>
-                        <p className=" m-2">Contact Us</p>
+                    <div className="m-3">
+                        <h1 className="text-lg font-semibold">General</h1>
+
+                        <div className="text-[#B2BEB5]">
+                            <p className="m-2">About Us</p>
+                            <p className="m-2">Contact Us</p>
+
+                            <div className="flex m-2">
+                                <TfiEmail className="my-auto"/>
+                                <span className="my-auto ms-2">info@aeroskymart.com</span>
+                            </div>
+
+                            <div className="flex m-2">
+                                <PiPhoneCallThin className="my-auto"/>
+                                <span className="my-auto ms-2">+254712345678</span>
+                            </div>
+                        </div>
 
                     </div>
 
                     <div className="m-3">
-                        <h1>General</h1>
-
-                        <p className=" m-2">About Us</p>
-                        <p className=" m-2">Contact Us</p>
-
-                        <div className="flex m-2">
-                            <TfiEmail className="my-auto"/>
-                            <span className="my-auto ms-2">info@aeroskymart.com</span>
-                        </div>
-
-                        <div className="flex m-2">
-                            <PiPhoneCallThin className="my-auto"/>
-                            <span className="m-auto ms-2">+254712345678</span>
-                        </div>
-
-                    </div>
-
-                    <div className="m-3">
-                        <h1>Socials</h1>
+                        <h1 className="text-lg font-semibold">Socials</h1>
 
                         <div className="flex flex-wrap m-2">
                             {socials.map((social, index)=> {
