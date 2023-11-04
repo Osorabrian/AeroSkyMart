264ca6dc3f9e9a3883776db772a131a2e1a275cf
Osorabrian <osorabrian@gmail.com>
2023-11-04 20:06:53 +0300

    edit socials section in footer

--- src/Components/Footer/Socials.js
+++ src/Components/Footer/Socials.js
@@ -21,7 +21,7 @@ export default function Socials(){
                 {socials.map((social, index)=> {
                     return(
                         <Link to={social.link} key={index}>
-                            <social.logo className={`text-[${social.color}] my-auto m-2 text-xl`} />
+                            <social.logo className={`text-[${social.color}] my-auto me-3 mb-2 mt-1 text-xl`} />
                         </Link>
                     )
                 })} 
