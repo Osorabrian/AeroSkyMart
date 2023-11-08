2d1932857ae1554cb756ef5a0e61341ead404ff7
Osorabrian <osorabrian@gmail.com>
2023-11-08 10:23:41 +0300

    format search section

--- src/Components/Store/store.js
+++ src/Components/Store/store.js
@@ -62,12 +62,22 @@ export default function Store(){
         <div className="font-lato" id='store'>
              
             { !filter && (
-                <div className="row mt-3 mb-3" id='search-row'>
-                    <div className="row ms-auto" id='search-bar'>
-                        <input placeholder="Search..." type="search" className="form-control w-50 rounded-0 col-9 ms-11" id='search' onChange={e => setSearch(e.target.value)}/>
-                        <button className="text-right col-3 -mt-1" onClick={handleFilter}><i className="fa-solid fa-filter mt-3" style={{color: "#e96f0c"}}> </i>   Filter</button>
+                    <div className="flex mx-auto m-3" id='search-section'>
+
+                        <input
+                         placeholder="Search..."
+                         type="search" 
+                         className="form-control rounded-0 h-10 my-auto" 
+                         id='search' 
+                         onChange={e => setSearch(e.target.value)}
+                        />
+
+                        <button className="flex my-auto m-3" onClick={handleFilter}>
+                            <i className="fa-solid fa-filter me-1 my-auto" style={{color: "#e96f0c"}}></i>   
+                            Filter
+                        </button>
+
                     </div>
-                </div>
             )}
 
             {!loading ? (
