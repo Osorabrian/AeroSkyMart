28d793bd3a81f3e0f412f38513a209d18f189090
Osorabrian <osorabrian@gmail.com>
2023-11-09 15:50:57 +0300

    format productTile

--- src/Components/Store/ProductTile.js
+++ src/Components/Store/ProductTile.js
@@ -7,7 +7,7 @@ import {Link} from 'react-router-dom'
 
 export default function ProductTile({name, id}){
     return(
-        <div>
+        <div className="mb-3" id='project-tile'>
             <Link id='tile' to={`${id}`}>
                 <div className="flex">
                     <img src={thumbnail} alt={'gulfstream'} id='thumbnail'/>
