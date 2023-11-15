9ec32048c105b193cd5a5371b375247c929e85bd
Osorabrian <osorabrian@gmail.com>
2023-11-15 10:36:38 +0300

    format forgot password page

--- src/Components/Login/Forgot Password.js
+++ src/Components/Login/Forgot Password.js
@@ -10,13 +10,14 @@ export default function ForgotPassword(){
     return(
         <div className="mb-5">
 
-            <h1 className="justify-center flex flex-wrap text-4xl font-semibold mt-4">Forgot Password ?</h1>
+            <h1 className="justify-center flex flex-wrap text-4xl font-semibold mt-5">Forgot Password ?</h1>
 
-            <CiCircleCheck className='mx-auto my-4 text-8xl fill-[rgb(0,255,0)]'/>
+            <CiCircleCheck className='mx-auto my-5 text-8xl fill-[rgb(0,255,0)]'/>
 
-            <p className="justify-center flex flex-wrap">We have sent you an email to your email
-             <p className="text-[#0284c7] ms-1 me-1">{email}</p> 
-             to allow you reset your password.
+            <p className="justify-center flex flex-wrap px-2">
+                We have sent you an email to your email
+                <span className="text-[#0284c7] ms-1 me-1">{email}</span>
+                to allow you reset your password.
             </p>
 
         </div>
