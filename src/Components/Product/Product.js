d5f55949eb584075542331bf61c768e1ac31a05b
Osorabrian <osorabrian@gmail.com>
2023-11-20 11:14:48 +0300

    format image chevrons

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import './product.css'
-import {FcPrevious, FcNext} from 'react-icons/fc'
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 import img1 from '../images/msfs/msfs_cover.jpg'
 import img2 from '../images/msfs/msfs_cockpit.jpg'
 import img3 from '../images/msfs/msfs_2.jpg'
@@ -25,13 +25,9 @@ export default function Product(){
                 <div className="carousel-image">
                     <img src={images[currentImage]} alt="msfs" className="mt-4 p-3"/>
 
-                    <button type='button' className="next-button" onClick={() => {setCurrentImage(currentImage + 1)}}>
-                        <FcNext className="text-4xl hover:scale-150"/>
-                    </button>
+                    <BsChevronRight className="next-button text-6xl cursor-pointer" onClick={() => {setCurrentImage(currentImage + 1)}}/>
 
-                    <button type='button' className="prev-button" onClick={() => setCurrentImage(currentImage - 1)}>
-                        <FcPrevious className="text-4xl hover:scale-150"/>
-                    </button>
+                    <BsChevronLeft className="prev-button text-6xl cursor-pointer" onClick={() => setCurrentImage(currentImage - 1)}/>
                     
                     <p className='image-numbers'>{currentImage + 1}/{images.length}</p>
                 </div>
