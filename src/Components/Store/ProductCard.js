0de4eb538c54737e56edac86ac8058648c5d7ae0
Osorabrian <osorabrian@gmail.com>
2023-11-22 16:40:07 +0300

    format product card

--- src/Components/Store/ProductCard.js
+++ src/Components/Store/ProductCard.js
@@ -1,7 +1,6 @@
 import React from "react";
-import {BsCheck2} from 'react-icons/bs'
-import {HiOutlineX} from 'react-icons/hi'
-import {GiCargoShip} from 'react-icons/gi'
+import {AiFillStar,AiOutlineStar} from 'react-icons/ai'
+import {BsStarHalf} from 'react-icons/bs'
 import './productcard.css'
 import {Link} from 'react-router-dom'
 import gulf from '../images/gulfstream.jpg'
@@ -13,10 +12,17 @@ export default function ProjectCard({name,id}){
             <img src={gulf} alt='lt' className="card-img-top px-0 flex-0"/>
             <div className="card-body text-left -ms-3 -mt-2">
                 <p className='card-text mb-1 font-semibold' id='card-title'>{name}</p>
-                <div className="row mb-1">
-                    {/* <BsCheck2 className="fill-[#4caf50] col-3 mt-1 -ml-3 icon"/><p className="col-9 -ml-6 card-text stock">5 in Stock</p> */}
-                    <HiOutlineX className="text-[rgb(255,0,0)] col-3 mt-1 -ml-3 icon"/> <p className="col-9 -ml-6 card-text">Sold Out</p>
-                    {/* <GiCargoShip className="text-[#448aff] col-3 mt-1 -ml-3 icon"/><p className="col-9 -ml-6 card-text">Shipping</p> */}
+                <div className="flex flex-wrap my-2">
+                            <div className="flex my-auto">
+                                <AiFillStar className="checked" />
+                                <AiFillStar className="checked"/>
+                                <BsStarHalf className="checked"/>
+                                <AiOutlineStar className="unchecked"/>
+                                <AiOutlineStar className="unchecked"/>
+                            </div>
+                            <div>
+                                <em className="ms-2 tile-text">(96 reviews)</em>
+                            </div>
                 </div>
                 <h1 className='card-text font-semibold text-[#e65100]'>Kshs. 20,000</h1>
             </div>
