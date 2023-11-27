4947c213edae913e507ecd27bc5b9af88c0fbf44
Osorabrian <osorabrian@gmail.com>
2023-11-27 11:36:15 +0300

    format product page

--- src/Components/Product/PreviouslyViewed.js
+++ src/Components/Product/PreviouslyViewed.js
@@ -4,8 +4,8 @@ import ProductCard from '../Store/ProductCard'
 export default function PreviouslyViewed(){
     return(
         <div>
-                <h1 className="text-4xl font-semibold mt-3">Previously Viewed</h1>
-                <div className="flex flex-wrap my-3">
+                <h1 className="text-4xl font-semibold mt-1">Previously Viewed</h1>
+                <div className="flex flex-wrap mt-3 mb-2">
                     <ProductCard/>
                     <ProductCard/>
                     <ProductCard/>
--- src/Components/Product/SimilarProducts.js
+++ src/Components/Product/SimilarProducts.js
@@ -4,8 +4,8 @@ import ProductCard from '../Store/ProductCard'
 export default function SimilarProducts(){
     return(
             <div>
-                <h1 className="text-4xl font-semibold mt-3">Similar Products</h1>
-                <div className="flex flex-wrap my-3">
+                <h1 className="text-4xl font-semibold mt-4">Similar Products</h1>
+                <div className="flex flex-wrap mt-3 mb-2">
                     <ProductCard/>
                     <ProductCard/>
                     <ProductCard/>
