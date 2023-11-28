2070f5946a05920c652a1c8a12c64486fddd840e
Osorabrian <osorabrian@gmail.com>
2023-11-28 15:51:12 +0300

    add margin-y in previous and similar items divs

--- src/Components/Product/SimilarProducts.js
+++ src/Components/Product/SimilarProducts.js
@@ -14,7 +14,7 @@ export default function SimilarProducts(){
     return(
             <div>
                 <h1 className="text-4xl text-left font-semibold mt-4">Similar Products</h1>
-                <div className="flex flex-row space-x-5 ms-5 w-11/12 my-4 overflow-x-scroll">
+                <div className="flex flex-row space-x-5 ms-1 w-11/12 my-4 overflow-x-scroll">
                     {
                         cardList.slice(0,6).map((card) => {
                             return( 
