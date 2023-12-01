e2978296e9ea040c56de920d3650d3fb1d9fe6dd
Osorabrian <osorabrian@gmail.com>
2023-12-01 15:59:01 +0300

    format cart page

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -9,21 +9,20 @@ export default function CartItem(){
 
     return(
             <div className="mx-3">
-                <div className="flex">
+                <div className="flex text-left">
                     <img src={img} alt="" id="thumbnail"/>
 
                     <div className="flex-wrap my-auto mx-2">
-                        <p className="flex flex-wrap font-bold text-left">Microsoft Flight Simulator 2020</p>
-                        <div className='justify-evenly flex'>
+                        <p className="flex flex-wrap font-bold">Microsoft Flight Simulator 2020</p>
 
-                                <div className="flex">
-                                    <FiMinus className="my-auto m-1" onClick={() => setQuantity(quantity - 1)}/>
-                                    <input placeholder={quantity} value={quantity} className="border-1 border-[black] my-auto text-center" id='expandable-input'/>
-                                    <GoPlus className="my-auto m-1" onClick={() => setQuantity(quantity + 1)}/>
-                                </div>
-
-                            <p className="text-lg text-[#ff9800] ms-1 font-semibold">Kshs.280,000</p>
+                        <div className="flex my-1">
+                            <p className="me-2">Quantity:</p>
+                            <FiMinus className="my-auto m-1" onClick={() => setQuantity(quantity - 1)}/>
+                            <input placeholder={quantity} value={quantity} className="border-1 border-[black] my-auto text-center" id='expandable-input'/>
+                            <GoPlus className="my-auto m-1" onClick={() => setQuantity(quantity + 1)}/>
                         </div>
+
+                        <p className="text-lg text-[#ff9800] font-semibold">Kshs.280,000</p>
                     </div>
                 </div>
                 <hr className="my-2"></hr>
