b0d02bc67209e8ede1566dbb91e5ae9ce950bb3a
Osorabrian <osorabrian@gmail.com>
2023-12-09 18:29:20 +0300

    format cart.js

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -21,108 +21,109 @@ export default function Cart(){
                     <CartItem />
                 </div>
 
-                <div className="mx-2" id='pricing-shopping'>
+                <div className="flex-wrap" id='checkout-div'>
+                    <div id="pricing-shopping">
+                        <div className="text-left m-3">
+                            <h1 className="font-bold text-4xl">Shipping Info.</h1>
 
-                    <div className="text-left m-3">
-                        <h1 className="font-bold text-4xl">Shipping Info.</h1>
+                            <h2 className="font-semibold mt-2">Shipping Address</h2>
 
-                        <h2 className="font-semibold mt-2">Shipping Address</h2>
+                            <form name="shipping-address" className="flex-wrap">
 
-                        <form name="shipping-address" className="flex-wrap">
+                                <div className="m-2">
+                                    <input type="radio" name='shipping-address' value={'enter-address'} onClick={(e) => setAddressValue(e.target.value)}/>
+                                    <label className="ms-1">Enter shipping address.</label>
 
-                            <div className="m-2">
-                                <input type="radio" name='shipping-address' value={'enter-address'} onClick={(e) => setAddressValue(e.target.value)}/>
-                                <label className="ms-1">Enter shipping address.</label>
-
-                                {addressValue === 'enter-address' && (
-                                    <div>
-                                        <label>Country</label>
-                                        <input type="text" placeholder="Country"/>
-                                        <label>City</label>
-                                        <input type="text" placeholder="City"/>
-                                        <label>Postal Address</label>
-                                        <input type="text" placeholder="Postal Adress"/>
-                                        <label>House Number</label>
-                                        <input type="text" placeholder="House Number"/>
-                                        <label>Comments</label>
-                                        <textarea placeholder="Additional Comments" rows={2}/>
-                                    </div>
-                                )}
-                            </div>
-                            
-                            <div className="m-2">
-                                <input type="radio" name="shipping-address" value={'saved-address'} onClick={(e) => setAddressValue(e.target.value)}/>
-                                <label className="ms-1">Ship to saved address.</label>   
-
-                                {
-                                    addressValue === 'saved-address' && (
+                                    {addressValue === 'enter-address' && (
                                         <div>
-                                            <form>
-                                                <label>Select Address</label>
-                                                <select name="address" >
-                                                    <option value={'home'}>Home</option>
-                                                    <option value={'work'}>Work</option>
-                                                    <option value={'parents'}>Parents</option>
-                                                </select>
-                                            </form>
+                                            <label>Country</label>
+                                            <input type="text" placeholder="Country"/>
+                                            <label>City</label>
+                                            <input type="text" placeholder="City"/>
+                                            <label>Postal Address</label>
+                                            <input type="text" placeholder="Postal Adress"/>
+                                            <label>House Number</label>
+                                            <input type="text" placeholder="House Number"/>
+                                            <label>Comments</label>
+                                            <textarea placeholder="Additional Comments" rows={2}/>
                                         </div>
-                                    )
-                                }
-                            </div>
+                                    )}
+                                </div>
+                                
+                                <div className="m-2">
+                                    <input type="radio" name="shipping-address" value={'saved-address'} onClick={(e) => setAddressValue(e.target.value)}/>
+                                    <label className="ms-1">Ship to saved address.</label>   
 
-                            <div className="m-2">
-                                <input type="radio" name="shipping-address" value={'current-address'}onClick={(e) => setAddressValue(e.target.value)}/>
-                                <label className="ms-1">Ship to current location.</label>
+                                    {
+                                        addressValue === 'saved-address' && (
+                                            <div>
+                                                <form>
+                                                    <label>Select Address</label>
+                                                    <select name="address" >
+                                                        <option value={'home'}>Home</option>
+                                                        <option value={'work'}>Work</option>
+                                                        <option value={'parents'}>Parents</option>
+                                                    </select>
+                                                </form>
+                                            </div>
+                                        )
+                                    }
+                                </div>
 
-                                {
-                                    addressValue === 'current-address' && (
-                                        <div>
-                                            <form>
-                                                <label>Current location</label>
-                                                <input type="text" placeholder="Current Location"/>
-                                            </form>
-                                        </div>
-                                    )
-                                }
-                            </div>
+                                <div className="m-2">
+                                    <input type="radio" name="shipping-address" value={'current-address'}onClick={(e) => setAddressValue(e.target.value)}/>
+                                    <label className="ms-1">Ship to current location.</label>
 
-                        </form>
-                    </div>
-                    
-                    <div className="text-left m-3">
+                                    {
+                                        addressValue === 'current-address' && (
+                                            <div>
+                                                <form>
+                                                    <label>Current location</label>
+                                                    <input type="text" placeholder="Current Location"/>
+                                                </form>
+                                            </div>
+                                        )
+                                    }
+                                </div>
 
-                        <p className="text-4xl font-bold">Pricing</p>
+                            </form>
+                        </div>
+                        
+                        <div className="text-left m-3">
+                            <p className="text-4xl font-bold">Pricing</p>
 
-                        <div className="justify-between flex my-1">
+                            <div className="justify-between flex my-1">
                             <p>SubTotal:</p>
                             <p>102,000</p>
-                        </div>
+                            </div>
 
-                        <div className="justify-between flex my-1">
+                            <div className="justify-between flex my-1">
                             <p>shipping:</p>
                             <p>10,000</p>
-                        </div>
+                            </div>
 
-                        <div className="justify-between flex my-1">
+                            <div className="justify-between flex my-1">
                             <p>Discount:</p>
                             <p>0</p>
+                            </div>
+
+                            <div className="justify-between flex my-1">
+                            <p className="font-semibold text-2xl">Total:</p>
+                            <p className="font-semibold text-[#ff9800] text-2xl">Kshs. 112,000</p>
+                            </div>
+
+                            
+
                         </div>
 
-                        <div className="justify-between flex my-1">
-                            <p className="font-semibold text-xl">Total:</p>
-                            <p className="font-semibold text-[#ff9800] text-xl">Kshs. 112,000</p>
-                        </div>
-
-                        <div className="justify-center mx-auto my-3">
-                            <Link className="btn" id="checkout-btn" to={'/checkout'}>
+                    </div>
+                        <div className="my-3" id='checkout button'>
+                            <Link className="justify-center btn w-10/12" id="checkout-btn" to={'/checkout'}>
                                 Check Out
                                 <i className="fa-solid fa-bag-shopping ms-4 my-auto" style={{color: "#ffffff"}}></i>
                             </Link>
                         </div>
-
-                    </div>
-
-                </div>
+                </div>   
             </div>
         </div>
     )
