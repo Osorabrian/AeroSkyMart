ad94b635a8fa2c37ecd889ebe1546178c796f4fd
Osorabrian <osorabrian@gmail.com>
2024-01-10 11:13:57 +0300

    add borders and align the divs

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { BsPaypal } from "react-icons/bs";
 import { PiCreditCardThin } from "react-icons/pi";
-import { FaApplePay } from "react-icons/fa";
+import { FaApple } from "react-icons/fa6";
 import { FcGoogle } from "react-icons/fc";
 import mpesa from '../images/mpesa logo.png'
+import './checkout.css'
 
 export default function Checkout(){
 
@@ -11,20 +12,21 @@ export default function Checkout(){
         <div>
             <p>Kshs. 1,2000,000</p>
             <h1 className='my-4'>Payment Type</h1>
-            <div>
 
-                <div className='flex'>
-                    <input type='radio' name='payment' />
-                    <label className='flex'>
-                        <PiCreditCardThin className='my-auto'/>
+            <div id='payment-types'>
+
+                <div className='flex border-1 border-[black] my-2 p-2'>
+                    <input type='radio' name='payment'/>
+                    <label className='flex ms-2'>
+                        <PiCreditCardThin className='my-auto me-1'/>
                         Debit/Credit Card
                     </label>
                 </div>
 
-                <div className='flex'>
+                <div className='flex border-1 border-[#253b80] my-2 p-2'>
                     <input type='radio' name='payment'/>
-                    <label className='flex'>
-                        <BsPaypal className='my-auto fill-[#253b80]'/>
+                    <label className='flex ms-2'>
+                        <BsPaypal className='my-auto fill-[#253b80] me-1'/>
                         <div>
                             <i className='text-[#253b80] font-bold'>pay</i>
                             <i className='text-[#179bd7] font-bold'>Pal</i>
@@ -32,25 +34,26 @@ export default function Checkout(){
                     </label>
                 </div>
 
-                <div className='flex'>
+                <div className='flex border-1 border-[#228B22] my-2 p-2'>
                     <input type='radio' name='payment'/>
-                    <label>
-                        <img src={mpesa} alt='mpesa' style={{height: '60px'}} />
+                    <label className='ms-2'>
+                        <img src={mpesa} alt='mpesa' style={{height: '35px'}} />
                     </label>
                 </div>
 
-                <div className='flex'>
+                <div className='flex border-1 border-[black] my-2 p-2'>
                     <input type='radio' name='payment'/>
-                    <label>
-                        <FaApplePay className='text-5xl'/>
+                    <label className='flex ms-2'>
+                        <FaApple className='text-lg me-1'/>
+                        Apple Pay
                     </label>
                 </div>
 
-                <div className='flex'>
+                <div className='flex border-1 border-[#4285F4] my-2 p-2'>
                     <input type='radio' name='payment'/>
-                    <label className='flex'>
-                        <FcGoogle className='my-auto'/>
-                        Pay
+                    <label className='flex ms-2'>
+                        <FcGoogle className='my-auto text-lg me-1'/>
+                        Google Pay
                     </label>
                 </div>
 
