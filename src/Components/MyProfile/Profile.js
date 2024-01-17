694ac5ab5fd850089824c67e293193db76837b03
Osorabrian <osorabrian@gmail.com>
2024-01-17 13:33:05 +0300

    create my profile component

--- src/Components/MyProfile/Profile.js
+++ src/Components/MyProfile/Profile.js
@@ -10,9 +10,27 @@ export default function Profile(){
                 <img src={profile_pic} alt='profile pic' id='profile-picture'/>
                 <MdAddAPhoto id='change-profile-picture-icon' className='text-2xl fill-[#ff9800]'/>
             </div>
+            <div className='text-left ms-4'>
 
-            <p className='text-2xl font-bold'>Brian Osora</p>
+                <p className='text-4xl font-bold'>Brian Osora</p>
 
+                <div className='my-2'>
+                    <p className='text-[grey] text-bold'>Email</p>
+                    <p className='font-semibold'>osorabrian@gmail.com</p>
+                </div>
+
+                <div className='my-2'>
+                    <p className='text-[grey] text-bold'>Phone Number</p>
+                    <p className='font-semibold'>+254746451952</p>
+                </div>
+
+                <div className='my-2'>
+                    <p className='text-[grey] text-bold'>Address </p>
+                    <p className='font-semibold'>Home</p>
+                    <p>Nairobi, South C, Kawi House.</p>
+                </div>
+
+            </div>
         </div>
     )
 }
