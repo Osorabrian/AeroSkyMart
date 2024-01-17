48700584eb5a4b05c6fe6c1809beaf4b7ad4a920
Osorabrian <osorabrian@gmail.com>
2024-01-17 11:46:23 +0300

    create my profile component

--- src/Components/MyProfile/Profile.js
+++ src/Components/MyProfile/Profile.js
@@ -6,10 +6,13 @@ import profile_pic from '../images/greencqard (2).JPG'
 export default function Profile(){
     return(
         <div>
-            <div id='profile-picture-div'>
+            <div id='profile-picture-div' className='my-4'>
                 <img src={profile_pic} alt='profile pic' id='profile-picture'/>
                 <MdAddAPhoto id='change-profile-picture-icon' className='text-2xl fill-[#ff9800]'/>
             </div>
+
+            <p className='text-2xl font-bold'>Brian Osora</p>
+
         </div>
     )
 }
