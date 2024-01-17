79cad46268abe89a30beead8675a7112bc9bd597
Osorabrian <osorabrian@gmail.com>
2024-01-17 16:37:29 +0300

    make input accept only images

--- src/Components/MyProfile/EditProfile.js
+++ src/Components/MyProfile/EditProfile.js
@@ -3,21 +3,47 @@ import React from 'react'
 export default function EditProfile(){
     return(
         <div>
-            <form>
-                <label>First Name</label>
-                <input type='text' placeholder='first name'/>
 
-                <label>Last Name</label>
-                <input type='text' placeholder='last name'/>
+            <h1 className='text-3xl font-bold mt-4'>Edit Profile</h1>
 
-                <label>Email</label>
-                <input type='email' placeholder='example@yahoo.com'/>
+            <form className='text-left m-3'>
 
-                <label>Phone Number</label>
-                <input type='number' placeholder='0746451952'/>
+                <div className='flex my-4'>
 
-                <label>Adress</label>
-                <input type='text' />
+                    <div className='me-3'>
+                        <label className='form-label'>First Name</label>
+                        <input type='text' placeholder='first name' className='form-control'/>
+                    </div>
+
+                    <div>
+                        <label className='form-label'>Last Name</label>
+                        <input type='text' placeholder='last name' className='form-control'/>
+                    </div>
+
+                </div>
+
+                <div className='flex my-4'>
+                    <div className=''>
+                        <label className='form-label'>Email</label>
+                        <input type='email' placeholder='example@gmail.com'className='form-control'/>
+                    </div>
+
+                    <div className='ms-3'>
+                        <label className='form-label'>Phone Number</label>
+                        <input type='number' placeholder='0746451952' className='form-control'/>
+                    </div>
+                </div>
+
+                <div className='my-4'>
+                    <label className='form-label'>Adress</label>
+                    <input type='text' placeholder='Enter Adress' className='form-control'/>
+                </div>
+
+                <div className='justify-between flex mx-5 my-5'>
+                    <button className='btn btn-danger rounded-none'>Cancel</button>
+                    <button className='btn btn-success rounded-none'>Update</button>
+                </div>
+
             </form>
         </div>
     )
