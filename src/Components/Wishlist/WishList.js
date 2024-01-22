e23fbac4f77bc90dc9f30a03fefd11689dfbd391
Osorabrian <osorabrian@gmail.com>
2024-01-22 16:12:29 +0300

    add button to direct to store

--- src/Components/Wishlist/WishList.js
+++ src/Components/Wishlist/WishList.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react'
 import ProductCard from '../Store/ProductCard'
 import './wishlist.css'
+import { FaOpencart } from "react-icons/fa";
+import { Link } from 'react-router-dom'
 
 export default function WishList(){
 
@@ -11,11 +13,11 @@ export default function WishList(){
     const [lastPost, setLastPost] = useState(20)
     const [currentPage, setCurrentPage] = useState(1)
     
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/albums')
-        .then(res => res.json())
-        .then(data => setList(data))
-    },[])
+    // useEffect(() => {
+    //     fetch('https://jsonplaceholder.typicode.com/albums')
+    //     .then(res => res.json())
+    //     .then(data => setList(data))
+    // },[])
 
     const data = list.slice(firstPost, lastPost).map((item) => {
         return (
@@ -37,25 +39,44 @@ export default function WishList(){
 
     return(
         <>
-            <p className='text-4xl my-4'>({list.length}) items</p>
+            {list.length === 0 ? (
+                <div id='no-items'>
 
-            <div className='row m-3'>
-                { data }
-            </div>
+                    <p className='my-5 mx-2'>
+                        You have not added items to your wishlist, you can visit <Link to={'/store'} className='text-[#ff9800]'>our store </Link> to add items into your wishlist.
+                    </p>
 
-            <div className='mx-auto' id='pagination'>
-                <div className='justify-between flex mx-5 mb-4'>
-                    <button onClick={() => previous()} className='btn border-1 bg-black text-white px-3'>
-                        Prev
-                    </button>
+                    <Link className='mx-auto justify-center flex border-1 bg-[#ff9800] rounded-none mt-5 mb-5 text-[white] p-3 w-6/12' to={'/store'}>
+                        <FaOpencart className='me-2'/>
+                        Proceed to Store
+                    </Link>
 
-                    <p className='my-auto'>Page {currentPage} of {totalPages}</p>
-
-                    <button onClick={() => {next()}} className='btn border-1 bg-black text-white px-3'>
-                        Next
-                    </button>
                 </div>
-            </div>
+            ):(
+                <div>
+
+                    <p className='text-4xl my-4'>({list.length}) items</p>
+
+                    <div className='row m-3'>
+                        { data }
+                    </div>
+
+                    <div className='mx-auto' id='pagination'>
+                        <div className='justify-between flex mx-5 mb-4'>
+                            <button onClick={() => previous()} className='btn border-1 bg-black text-white px-3'>
+                                Prev
+                            </button>
+
+                            <p className='my-auto'>Page {currentPage} of {totalPages}</p>
+
+                            <button onClick={() => {next()}} className='btn border-1 bg-black text-white px-3'>
+                                Next
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
+
         </>
     )
 }
