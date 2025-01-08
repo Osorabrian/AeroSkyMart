77c5a9e9ebdc71eb82925b0f99a8841e42319ab7
Osorabrian <osorabrian@gmail.com>
2025-01-08 11:44:52 -0800

    format go to store button

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -14,7 +14,7 @@ export default function Home() {
 
                    <h1 className="text-light font-semibold font-raleway leading-normal mt-4" id='message'>Elevate Your Aviation Experience!</h1>
 
-                    <Link type="button" className="bg-white p-3 mt-4 " to='/store'>
+                    <Link type="button" className="bg-white p-3 mt-5" to='/store'>
                         Go To Store
                     </Link> 
 
