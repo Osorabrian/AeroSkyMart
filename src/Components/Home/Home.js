6eb1b2c98c96c515f02638620488a361a65ea15c
Osorabrian <osorabrian@gmail.com>
2025-01-08 11:19:34 -0800

    format go to store button

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -15,13 +15,13 @@ export default function Home() {
 
                    <h1 className="text-light font-semibold font-raleway leading-normal mt-4" id='message'>Elevate Your Aviation Experience!</h1>
 
-                    <Link type="button" to='/store'>
+                    <Link type="button" className="bg-white p-3" to='/store'>
                         <div className="row font-lato font-4xl">
                             <span className="col-9">
                                 Go To Store  
                             </span>
-                            <span>
-                                <FaOpencart className="col-3 mt-1 -ml-1"/>
+                            <span className="col-3">
+                                <FaOpencart className="mt-1 -ml-1"/>
                             </span>
                         </div>
                     </Link> 
